0f54afc22b654fae3ed85b3d91d33af91b0bdd22
Maxim Nikolenko <maxim.nikolenko@gmail.com>
2008-12-28 17:50:58 +0200

    minor fixes in stylesheet and pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,21 +15,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
-
-  # HACK HACK HACK -- how to do attr_accessible from here?
-  # prevents a user from submitting a crafted form that bypasses activation
-  # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :password, :password_confirmation
 
-
-
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  #
-  # uff.  this is really an authorization, not authentication routine.  
-  # We really need a Dispatch Chain here or something.
-  # This will also let us return a human error message.
-  #
   def self.authenticate(login, password)
     return nil if login.blank? || password.blank?
     u = find_by_login(login) # need to get the salt
@@ -43,9 +30,5 @@ class User < ActiveRecord::Base
   def email=(value)
     write_attribute :email, (value ? value.downcase : nil)
   end
-
-  protected
-    
-
 
 end
